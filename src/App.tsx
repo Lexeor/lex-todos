91c8b356c6ee36a08dde642946391fdde424def1
Lexeor <lexeor.ross@gmail.com>
2023-08-14 11:58:25 +0400

    add axios + facade

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+import axios from './utils/fetch-facade';
+
 function App() {
-  return (
-    <div className="App">
-      todos
-    </div>
-  );
+  // Functions
+  const getTodos = async () => {
+    const res = await axios.get('/users/1/todos?_limit=5');
+    console.log(res);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return <div className="App">todos</div>;
 }
 
 export default App;
